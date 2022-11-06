fe239af8b907be286f76c8d906ce7a38c387e7aa
Eric Leo <likeju>
2022-11-06 23:58:50 +0800

    feat: 反射应用到数据源属性设置

--- src/main/java/org/kee/mybatis/datasource/unpooled/UnpooledDataSource.java
+++ src/main/java/org/kee/mybatis/datasource/unpooled/UnpooledDataSource.java
@@ -42,6 +42,16 @@ public class UnpooledDataSource implements DataSource {
         }
     }
 
+    public UnpooledDataSource() {
+    }
+
+    public UnpooledDataSource(String driver, String url, String username, String password) {
+        this.driver = driver;
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
     /**
      * 驱动代理
      */
--- src/main/java/org/kee/mybatis/datasource/unpooled/UnpooledDataSourceFactory.java
+++ src/main/java/org/kee/mybatis/datasource/unpooled/UnpooledDataSourceFactory.java
@@ -1,6 +1,8 @@
 package org.kee.mybatis.datasource.unpooled;
 
 import org.kee.mybatis.datasource.DataSourceFactory;
+import org.kee.mybatis.reflection.MetaObject;
+import org.kee.mybatis.reflection.SystemMetaObject;
 
 import javax.sql.DataSource;
 import java.util.Properties;
@@ -12,21 +14,57 @@ import java.util.Properties;
  */
 public class UnpooledDataSourceFactory implements DataSourceFactory {
 
-    protected Properties props;
+    protected DataSource dataSource;
+
+    public UnpooledDataSourceFactory() {
+        this.dataSource = new UnpooledDataSource();
+    }
 
     @Override
     public void setProperties(Properties props) {
-        this.props = props;
+        MetaObject metaObject = SystemMetaObject.forObject(dataSource);
+        for (Object prop : props.keySet()) {
+            String propName = (String) prop;
+            // 允许set才可以set
+            if (metaObject.hasSetter(propName)) {
+                String value = (String) props.get(propName);
+                Object convertValue = convertValue(metaObject, propName, value);
+
+                metaObject.setValue(propName, convertValue);
+            }
+        }
     }
 
     @Override
     public DataSource getDataSource() {
-        UnpooledDataSource unpooledDataSource = new UnpooledDataSource();
-        unpooledDataSource.setDriver(props.getProperty("driver"));
-        unpooledDataSource.setUrl(props.getProperty("url"));
-        unpooledDataSource.setUsername(props.getProperty("username"));
-        unpooledDataSource.setPassword(props.getProperty("password"));
-        return unpooledDataSource;
+        return dataSource;
+    }
+
+    /**
+     * 根据setter的类型,将配置文件中的值强转成相应的类型
+     * @param metaObject
+     * @param propName
+     * @param value
+     * @return
+     */
+    private Object convertValue(MetaObject metaObject, String propName, String value) {
+        // 目标值
+        Object convertValue = value;
+        // 目标值类型
+        Class<?> convertType = metaObject.getSetterType(propName);
+
+        // 强转为基本数据类型
+        if (convertType == Integer.class || convertType == int.class) {
+            convertValue = Integer.valueOf(value);
+        }
+        if (convertType == Long.class || convertType == long.class) {
+            convertValue = Long.valueOf(value);
+        }
+        if (convertType == Boolean.class || convertType == boolean.class) {
+            convertValue = Boolean.valueOf(value);
+        }
+
+        return convertValue;
     }
 
 }
